ab75393eac0211a456be6757e6f396241fe187a7
Ronny Álvarez <ronny230797@gmail.com>
2021-05-05 19:54:30 -0600

    Add the correct number to the list

--- app/components/AddNumber.js
+++ app/components/AddNumber.js
@@ -1,29 +1,74 @@
-import React from 'react';
-import { Text, View, StyleSheet, Image,TextInput } from 'react-native';
+import React, { useState }  from 'react';
+import { Text, View, StyleSheet, Image, TextInput, Picker,Button,Alert  } from 'react-native';
+import {addNumber,removeNumber} from '../store/Actions';
+import { useSelector,useDispatch } from 'react-redux';
 
 const AddNumber = (props) => {
-    return (
+  const dispatch = useDispatch();
+  const [number, setNumber] = useState();
+  const [amount, setAmount] = useState();
+  const [dayNaight, setDayNaight] = useState();
+
+  let numberElement = {
+    number: number,
+    amount: amount,
+    dayNight: dayNaight
+};
+
+// const createTwoButtonAlert = (object) =>
+// Alert.alert(
+//   "Alert Title",
+//   `${object.number} ${object.amount} ${object.dayNight}`,
+//   [
+//     {
+//       text: "Cancel",
+//       onPress: () => console.log("Cancel Pressed"),
+//       style: "cancel"
+//     },
+//     { text: "OK", onPress: () => console.log("OK Pressed") }
+//   ]
+// );
+
+
+  return (
     <View>
-        <Text>Número</Text>
-        <TextInput
+      <TextInput
         style={styles.input}
-        // onChangeText={onChangeNumber}
-        // value={number}
+        onChangeText={setNumber}
+        value={number}
         placeholder="Número"
         keyboardType="numeric"
         maxLength={2}
       />
+      <TextInput
+        style={styles.input}
+        onChangeText={setAmount}
+        value={amount}
+        placeholder="Monto"
+        keyboardType="numeric"
+        maxLength={5}
+      />
+      <Picker
+        selectedValue={dayNaight}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setDayNaight(itemValue)}
+      >
+        <Picker.Item label="Noche" value="Night" />
+        <Picker.Item label="Día" value="Day" />
+      </Picker>
+      <Button title='Add' onPress={() => dispatch(addNumber(numberElement))} />
+      {/* <Button title={"2-Button Alert"} onPress={createTwoButtonAlert(numberElement)} /> */}
     </View>
-    )
+  )
 }
 
 export default AddNumber;
 
 const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-    },
-  });
-  
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+});
+
--- app/components/SelectedNumbers.js
+++ app/components/SelectedNumbers.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button, Image } from 'react-native';
 import { useSelector,useDispatch } from 'react-redux';
-import {addNumber,removeNumber} from '../store/Actions';
 
 const styles = StyleSheet.create({
     background: {
@@ -22,15 +21,10 @@ const styles = StyleSheet.create({
 
 const SelectedNumbers = (props) => {
     const data = useSelector(state => state.listOfNumbers)
-    const dispatch = useDispatch();
-    let number = {
-        number: 22,
-        amount: 987,
-        dayNight: 'Night'
-    };
+
     return (
         <View style={styles.background}>
-            <Button title='Add' onPress={() => dispatch(addNumber(number))} />
+            
             {
                 data.map(element =>
                     <View style={styles.container} key={element.number}>
